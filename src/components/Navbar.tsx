757935c47145a974999f943aa03f1363d39ec99d
Emmanuel López <emmlop243@gmail.com>
2022-04-16 20:14:16 -0500

    se colocó el big calendar react en CalendarScreen.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,6 @@
 export const Navbar = () => {
   return (
-    <nav className="sticky top-0 bg-slate-800 text-white py-3 px-8 flex justify-between items-center">
+    <nav className="sticky top-0 bg-slate-800 text-white py-3 px-8 flex justify-between items-center z-10">
       <span className="font-semibold">Emmanuel</span>
       <button className="">
         <i className="fas fa-sign-out"></i> &nbsp;
--- src/modules/calendar/CalendarScreen.tsx
+++ src/modules/calendar/CalendarScreen.tsx
@@ -1,7 +1,25 @@
+import { Calendar, momentLocalizer } from 'react-big-calendar';
+import moment from 'moment';
+
+import 'react-big-calendar/lib/sass/styles.scss';
+
 export const CalendarScreen = () => {
+  // Setup the localizer by providing the moment (or globalize, or Luxon) Object
+  // to the correct localizer.
+  const localizer = momentLocalizer(moment);
+  const events = [
+    {
+      title: 'Cumpleaños del jefe',
+      start: moment().toDate(),
+      end: moment().add(2, 'hours').toDate(),
+      bgColor: '#fafafa',
+    },
+  ];
+
   return (
-    <div>
+    <div className="h-[80vh] w-full">
       <h1>Calendar Screen</h1>
+      <Calendar localizer={localizer} events={events} startAccessor="start" endAccessor="end" />
     </div>
   );
 };
